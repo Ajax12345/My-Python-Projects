36ac40a77be21578252747736f9cb1dc8a9bd8e3
James Petullo <pentester18@gmail.com>
2017-06-14 10:16:20 -0400

    Create Pig_Latin_Translator.py

--- Pig_Latin_Translator.py
+++ Pig_Latin_Translator.py
@@ -4,15 +4,8 @@ def main():
 
 
 def piglatin(first_word):
-  vowels = ["a", "e", "i", "o", "u"]
-
-  word = first_word.split(" ")
-  for i in word:
-    if i[:1] not in vowels:
-      new_word = i[1:]+i[:1]+"ay"
-      print(new_word)
-    else:
-      new_word = i[1:]+"hay"
-      print(new_word)
+  
+  
+    print ' '.join(i+"ay" if i.lower() == "i" else i[1:]+i[0]+"ay" for i in first_word.split())
 
 main()
