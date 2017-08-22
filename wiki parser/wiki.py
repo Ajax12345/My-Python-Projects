753a5dc5bd152ad8a11d76773e9b55ed9eb62ba9
Ajax1234 <pentester18@gmail.com>
2017-08-21 21:04:35 -0400

    Added support for source collection of article topic
    Wikiparser is a full web scrapper written in Python to allow users to access Wikipedia article summaries and sources.

--- wiki parser/wiki.py
+++ wiki parser/wiki.py
@@ -2,18 +2,18 @@ import urllib
 import re
 import collections
 import itertools
-
+#TODO: build method to garner sources from wikepdia articles
 class Wiki:
-    def __init__(self, query):
-        self.query = query
+    def __init__(self):
+        #self.query = query
         self.topics = None
-        self.disambiguation()
+        #self.disambiguation()
         self.seen_proper_href = False
         self.seen_proper_title = False
 
-    def disambiguation(self):
+    def disambiguation(self, keyword):
         data_info = collections.namedtuple("data_info", "extension1 name name1")
-        url = "https://en.wikipedia.org/wiki/{}_(disambiguation)".format(self.query)
+        url = "https://en.wikipedia.org/wiki/{}_(disambiguation)".format(keyword)
         data = str(urllib.urlopen(url).read())
         final_data = re.findall('<a href="(.*?)" title="(.*?)">(.*?)</a>', data)[6:-48]
         topics = map(data_info._make, final_data)
@@ -41,13 +41,66 @@ class Wiki:
         new_stuff = [i for i in new_stuff if i]
         return  ' '.join(new_stuff)
 
+    def get_article_sources(self, article_name, **kwargs):
+        url = None
+        if kwargs.get("full_url", False):
+            url = "https://en.wikipedia.org{}".format(article_name)
+
+        else:
+            url = "https://en.wikipedia.org/wiki/{}".format(article_name)
+
+        data = str(urllib.urlopen(url).read())
+        s = '<li id="cite_note-(.*?)">(.*?)</li>'
+        raw_sources = re.findall(s, data)
+        final_dict = collections.defaultdict(dict)
+
+        for number, raw_content in raw_sources:
+            citation_type = re.findall('<cite class="(.*?)">', raw_content)
+            final_dict[number]["citation_type"] = citation_type
+
+            testing_r = '<cite class="(.*?)">(.*?)\.'
+            author = re.findall(testing_r, raw_content)
+
+            author = "None" if not author else author
+            if author != "None":            #if "<a rel" not in re.findall(testing_r, raw_content)[-1] else "No Author"
+                new_author = author[0][-1]
+
+                if "<a rel" in new_author:
+                    author = "None"
+
+                else:
+                    author = new_author
+            final_dict[number]["Author"] = author
+
+            web_url = re.findall('href="(.*?)">', raw_content)
+            final_dict[number]["url"] = web_url[-1]
+            full_description = re.findall('>"(.*?)"</a>', raw_content)
+            final_dict[number]["description"] = full_description[0] if full_description else "None"
+
+            retrieved = re.findall('Retrieved <span class="(.*?)">(.*?)</span>(.*?)</span>\.', raw_content)
+            print "retrieved", "N/D" if not retrieved else ' '.join(retrieved[0][1:])
+            final_dict[number]["retrieved"] = "N/D" if not retrieved else ' '.join(retrieved[0][1:])
+
+        return final_dict
+
+
+
+
+
+
+
+
+
+
 
     def filter_html_and_non_alpha(self, word):
         new_word = re.sub("<(.*?)>", '', word)
         if "href" in new_word:
-            if "wiki" in new_word:
+            if "/wiki/" in new_word:
                 #new_word = re.findall("href='/wiki/'(.*?)", new_word)[0]
+
                 new_word = new_word[new_word.index("/wiki/")+6:]
+
                 self.seen_proper_href = True
 
             else:
@@ -95,8 +148,11 @@ class Wiki:
 
 
 
+w = Wiki()
+#print w.get_article_info_by_keyword("python programming")
+#titles = w.disambiguation()
 
-w = Wiki("calculus")
-titles = w.disambiguation()
-print titles[0].extension1
-print w.get_article_info_by_url(titles[3].extension1)
+#print [(i.extension1, i.name) for i in titles]
+#w.get_article_sources("/wiki/Python_(programming_language)", full_url= True)
+print w.get_article_info_by_keyword("Newsmax")
+print w.get_article_sources("Newsmax")
