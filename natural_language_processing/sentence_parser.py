d280d8ff54bd8463b62a616981ad9e84c2a7606d
Ajax1234 <pentester18@gmail.com>
2017-08-25 15:47:05 -0400

    Update sentence_parser.py

--- natural_language_processing/sentence_parser.py
+++ natural_language_processing/sentence_parser.py
@@ -4,6 +4,9 @@ import re
 import string
 import wiki
 import collections
+import logging
+import csv
+import os
 
 class Parser(practice_file1.SpeechBot):
     def __init__(self):
@@ -17,12 +20,14 @@ class Parser(practice_file1.SpeechBot):
         self.pronouns = self.get_pronouns()
         self.personal_pronouns = self.get_personal_pronouns()
         self.converter = {"nouns":self.nouns, "adjectives":self.adjectives, "verbs":self.verbs, "pronouns":self.pronouns, "adverbs":self.adverbs, "personal_pronouns":self.personal_pronouns}
-        self.scrape_main_wiki_page()
+        self.parse_sentence()
 
 
-    def parse_sentence(self, wiki_data):
-        pass
-
+    def parse_sentence(self):
+        types = ["nouns", "verbs", "adjectives", "adverbs", "pronouns"]
+        data = csv.reader(open('sentence_parts.csv'))
+        data_dicts = [{a:b.split() for a, b in zip(types, i)} for i in data]
+        
     def check_interal_files(self, full_key_word):
         pass
 
@@ -32,17 +37,30 @@ class Parser(practice_file1.SpeechBot):
 
 
     def scrape_main_wiki_page(self):
+        noun_default = collections.defaultdict(list)
+        verb_default = collections.defaultdict(list)
+        sentence_parts = open('sentence_parts.txt', 'a')
 
         f = [i.strip('\n') for i in open('wiki_article_list.txt')]
-        #print f
-        full_results = collections.defaultdict(list)
-        for url in f[:1]:
 
+
+        full_results = collections.defaultdict(list)
+        for url in f:
+
+            print "on url:", url
             full_web_data = self.w.get_article_info_by_url(url)
             new_data = [i.split() for i in full_web_data.split(".")]
             converter_1 = [{a:[i for i in b if i in map(str.lower, c)] for a, b in self.converter.items()} for c in new_data]
 
-            print converter_1
+            for sentence in converter_1:
+                sentence_parts.write("{}|{}|{}|{}|{}|{}\n".format(' '.join(sentence["nouns"]), " ".join(sentence["verbs"]), " ".join(sentence["adjectives"]), " ".join(sentence["adverbs"]), ' '.join(sentence["pronouns"]), " ".join(sentence["personal_pronouns"])))
+
+
+
+        sentence_parts.close()
+
+
+
 
 
 
